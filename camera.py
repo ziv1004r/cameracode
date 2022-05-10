befe6c3021cb4bda2eee43eaaedd04f133964a7a
ziv itzhak <97166786+ziv1004r@users.noreply.github.com>
2022-05-10 15:12:06 +0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,18 +1,192 @@
-import numpy as np
+# # import numpy as np
+# # import cv2
+# # import cv
+
+# # cap = cv2.VideoCapture(0)
+
+# # while True:
+# #     ret,img = cap.read()
+# #     cv2.imshow('Original ziv Video',img)
+# #     img2=cv2.flip(img,-1)
+# #     cv2.imshow('Flipped ziv video',img2)
+# #     k=cv2.waitKey(30) & 0xff
+# #     if k==27:
+# #         break
+# #     if cv2.waitKey(1) & 0xFF == ord('q'):
+# #         break
+# # cap.release()
+# # cv2.destroyAllWindows()
+
+# import cv2
+# import mediapipe as mp
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
+# mp_holistic = mp.solutions.holistic
+
+# # For static images:
+# IMAGE_FILES = []
+# with mp_holistic.Holistic(
+#     static_image_mode=True,
+#     model_complexity=2,
+#     enable_segmentation=True,
+#     refine_face_landmarks=True) as holistic:
+#   for idx, file in enumerate(IMAGE_FILES):
+#     image = cv2.imread(file)
+#     image_height, image_width, _ = image.shape
+#     # Convert the BGR image to RGB before processing.
+#     results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+#     if results.pose_landmarks:
+#       print(
+#           f'Nose coordinates: ('
+#           f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].x * image_width}, '
+#           f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].y * image_height})'
+#       )
+#     annotated_image = image.copy()
+#     # Draw segmentation on the image.
+#     # To improve segmentation around boundaries, consider applying a joint
+#     # bilateral filter to "results.segmentation_mask" with "image".
+#     condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+#     bg_image = np.zeros(image.shape, dtype=np.uint8)
+#     bg_image[:] = BG_COLOR
+#     annotated_image = np.where(condition, annotated_image, bg_image)
+#     # Draw pose, left and right hands, and face landmarks on the image.
+#     mp_drawing.draw_landmarks(
+#         annotated_image,
+#         results.face_landmarks,
+#         mp_holistic.FACEMESH_TESSELATION,
+#         landmark_drawing_spec=None,
+#         connection_drawing_spec=mp_drawing_styles
+#         .get_default_face_mesh_tesselation_style())
+#     mp_drawing.draw_landmarks(
+#         annotated_image,
+#         results.pose_landmarks,
+#         mp_holistic.POSE_CONNECTIONS,
+#         landmark_drawing_spec=mp_drawing_styles.
+#         get_default_pose_landmarks_style())
+#     cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+#     # Plot pose world landmarks.
+#     mp_drawing.plot_landmarks(
+#         results.pose_world_landmarks, mp_holistic.POSE_CONNECTIONS)
+
+# # For webcam input:
+# cap = cv2.VideoCapture(0)
+# with mp_holistic.Holistic(
+#     min_detection_confidence=0.5,
+#     min_tracking_confidence=0.5) as holistic:
+#   while cap.isOpened():
+#     success, image = cap.read()
+#     if not success:
+#       print("Ignoring empty camera frame.")
+#       # If loading a video, use 'break' instead of 'continue'.
+#       continue
+
+#     # To improve performance, optionally mark the image as not writeable to
+#     # pass by reference.
+#     image.flags.writeable = False
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     results = holistic.process(image)
+
+#     # Draw landmark annotation on the image.
+#     image.flags.writeable = True
+#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#     mp_drawing.draw_landmarks(
+#         image,
+#         results.face_landmarks,
+#         mp_holistic.FACEMESH_CONTOURS,
+#         landmark_drawing_spec=None,
+#         connection_drawing_spec=mp_drawing_styles
+#         .get_default_face_mesh_contours_style())
+#     mp_drawing.draw_landmarks(
+#         image,
+#         results.pose_landmarks,
+#         mp_holistic.POSE_CONNECTIONS,
+#         landmark_drawing_spec=mp_drawing_styles
+#         .get_default_pose_landmarks_style())
+#     # Flip the image horizontally for a selfie-view display.
+#     cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
+#     if cv2.waitKey(5) & 0xFF == 27:
+#       break
+# cap.release()
+
 import cv2
-import cv
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+# For static images:
+IMAGE_FILES = []
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+  for idx, file in enumerate(IMAGE_FILES):
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv2.flip(cv2.imread(file), 1)
+    # Convert the BGR image to RGB before processing.
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
+    # Print handedness and draw hand landmarks on the image.
+    print('Handedness:', results.multi_handedness)
+    if not results.multi_hand_landmarks:
+      continue
+    image_height, image_width, _ = image.shape
+    annotated_image = image.copy()
+    for hand_landmarks in results.multi_hand_landmarks:
+      print('hand_landmarks:', hand_landmarks)
+      print(
+          f'Index finger tip coordinates: (',
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+      )
+      mp_drawing.draw_landmarks(
+          annotated_image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+    cv2.imwrite(
+        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    if not results.multi_hand_world_landmarks:
+      continue
+    for hand_world_landmarks in results.multi_hand_world_landmarks:
+      mp_drawing.plot_landmarks(
+        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+
+# For webcam input:
 cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
 
-while True:
-    ret,img = cap.read()
-    cv2.imshow('Original ziv Video',img)
-    img2=cv2.flip(img,-1)
-    cv2.imshow('Flipped ziv video',img2)
-    k=cv2.waitKey(30) & 0xff
-    if k==27:
-        break
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
 cap.release()
-cv2.destroyAllWindows()
+
